d7ad486631bed8f1dac1ddf03d84b1252b2179af
AvramAlexandraIoana <37377341+AvramAlexandraIoana@users.noreply.github.com>
2021-05-31 13:30:38 +0300

    Added typescript to react native app

---
+++ App.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'IOS',
+  android: 'ANDROID',
+});
+
+export interface Props { }
+export interface State { }
+
+export default class App extends React.Component<Props, State> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Platforma:
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
